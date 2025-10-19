0f58122a74a3658ddd8b0ab7ec691609237a033b
Zoraver Malhotra <fvy1777@autuni.ac.nz>
2025-10-19 23:13:27 +1300

    Change appearance to include thresholds and dynamic scaling. Remove fixed ip address

--- api.py
+++ api.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify, send_from_directory
 from src.sensors import read_values
+from src.thresholds import TEMP_THRESHOLD, HUMIDITY_THRESHOLD, evaluate_sensor
 
 app = Flask(__name__)
 
-latest_data = {"temperature": None, "humidity": None}
-
 @app.route("/sensor-data", methods=["GET"])
 def get_sensor_data():
-    return jsonify(read_values()), 200
+    data = read_values()
+    
+    data["temp_status"] = evaluate_sensor(data.get("temperature"), *TEMP_THRESHOLD)
+    data["humidity_status"] = evaluate_sensor(data.get("humidity"), *HUMIDITY_THRESHOLD)
+    
+    return jsonify(data), 200
 
 @app.route("/")
 def serve_dashboard():
