0be69bc45be2c71b59fbd4a5d4474f27e78aa3ba
zoravermalhotra <zoravermalhotra@gmail.com>
2026-01-20 02:01:44 +1300

    fixed duplicate host cards

--- api.py
+++ api.py
@@ -1,8 +1,19 @@
 from flask import Flask, jsonify, send_from_directory, request
 from src.thresholds import TEMP_THRESHOLD, HUMIDITY_THRESHOLD, evaluate_sensor
 from src.sensor_db import get_recent_data, store_remote_data, get_latest_per_pi
+import socket
 
 app = Flask(__name__)
+
+def get_host_ip():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	try:
+		s.connect(("8.8.8.8", 80))
+		return s.getsockname()[0]
+	finally:
+		s.close()
+		
+HOST_IP = get_host_ip()
 
 @app.route("/live", methods=["GET"])
 def get_live():
@@ -18,6 +29,10 @@ def receive_remote_data():
         data = request.get_json()
         if not data:
             return jsonify({"error": "No data provided"}), 400
+        
+        #Anything coming from host machine becomes pi_id = "host"
+        if request.remote_addr == HOST_IP:
+            data["pi_id"] = "host"
         
         #Store the remote data in database
         store_remote_data(data)
